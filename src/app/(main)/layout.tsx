b0afb3c3e1f52e310213e0731290dc585f439c69
dhanushtheijas08 <dhanushtheijas7@gmail.com>
2025-01-13 22:28:17 +0530

    fix: protected route splash screen issue

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -6,6 +6,7 @@ import {
 } from "@/components/ui/sidebar";
 import { DashboardSidebar } from "@/components/dashboard-sidebar";
 import { ThemeToggle } from "@/components/theme-toggle";
+import { ProtectedRoute } from "@/components/protected-route";
 
 export default function MainLayout({
   children,
@@ -13,20 +14,22 @@ export default function MainLayout({
   children: React.ReactNode;
 }) {
   return (
-    <SidebarProvider>
-      <div className="flex h-screen w-full overflow-hidden">
-        <DashboardSidebar />
-        <SidebarInset className="flex-1 overflow-auto">
-          <header className="flex w-full  h-16 items-center gap-4 border-b bg-background  px-4 py-4 sm:px-6">
-            <SidebarTrigger />
-            <h1 className="text-lg font-semibold">Dashboard</h1>
-            <div className="ml-auto">
-              <ThemeToggle />
-            </div>
-          </header>
-          <main className="p-4 sm:p-6 md:p-8">{children}</main>
-        </SidebarInset>
-      </div>
-    </SidebarProvider>
+    <ProtectedRoute allowedRoles={["admin", "user"]}>
+      <SidebarProvider>
+        <div className="flex h-screen w-full overflow-hidden">
+          <DashboardSidebar />
+          <SidebarInset className="flex-1 overflow-auto">
+            <header className="flex w-full  h-16 items-center gap-4 border-b bg-background  px-4 py-4 sm:px-6">
+              <SidebarTrigger />
+              <h1 className="text-lg font-semibold">Dashboard</h1>
+              <div className="ml-auto">
+                <ThemeToggle />
+              </div>
+            </header>
+            <main className="p-4 sm:p-6 md:p-8">{children}</main>
+          </SidebarInset>
+        </div>
+      </SidebarProvider>
+    </ProtectedRoute>
   );
 }
--- src/app/(main)/users/page.tsx
+++ src/app/(main)/users/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { ProtectedRoute } from "@/components/protected-route";
 import { userColumns } from "@/components/users/user-columns";
 import { UsersTable } from "@/components/users/users-table";
 import { useFetchUsers } from "@/queries/useUsers";
@@ -6,20 +7,20 @@ import { useFetchUsers } from "@/queries/useUsers";
 const UsersPage = () => {
   const { data: usersData, isLoading } = useFetchUsers();
 
-  console.log(usersData);
-
   return (
-    <div className="space-y-4">
-      <h1 className="text-2xl font-bold sm:text-3xl">Users</h1>
-      {isLoading ? (
-        <p>Loading..</p>
-      ) : (
-        usersData &&
-        usersData.length !== 0 && (
-          <UsersTable columns={userColumns} data={usersData} />
-        )
-      )}
-    </div>
+    <ProtectedRoute allowedRoles={["admin", "user"]}>
+      <div className="space-y-4">
+        <h1 className="text-2xl font-bold sm:text-3xl">Users</h1>
+        {isLoading ? (
+          <p>Loading..</p>
+        ) : (
+          usersData &&
+          usersData.length !== 0 && (
+            <UsersTable columns={userColumns} data={usersData} />
+          )
+        )}
+      </div>
+    </ProtectedRoute>
   );
 };
 export default UsersPage;
